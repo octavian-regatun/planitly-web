006d19f58b91dd396fbbef5a5d7fe65702a317bc
Octavian Regatun <octavian.regatun@outlook.com>
2023-02-19 23:06:07 +0200

    feat: add nprogressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { type Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
 import { type AppType } from "next/app"
+import NextNProgress from "nextjs-progressbar"
 
 import { api } from "../utils/api"
 
-import "../styles/globals.css"
 import Head from "next/head"
+import "../styles/globals.css"
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -28,6 +29,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <meta property="og:image" content="/logo.svg" />
       </Head>
       <SessionProvider session={session}>
+        <NextNProgress />
         <Component {...pageProps} />
       </SessionProvider>
     </>
