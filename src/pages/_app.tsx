890d91c0c288795e096b6088ee4c1b57bd69460c
Octavian Regatun <octavian.regatun@outlook.com>
2023-02-19 22:48:36 +0200

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,28 @@ import { type AppType } from "next/app"
 import { api } from "../utils/api"
 
 import "../styles/globals.css"
+import Head from "next/head"
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>PlanITLY</title>
+        <meta property="og:title" content="PlanITLY" />
+        <meta
+          property="og:description"
+          content="Let's upgrade the way you plan things in your life. If you were looking for a way to manage all tasks, meetings, events,
+          trips in one place easier than ever? Well, you found the way now!"
+        />
+        <meta property="og:image" content="/logo.svg" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
 
