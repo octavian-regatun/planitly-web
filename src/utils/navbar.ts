519e579a28d89d9a29773245d53803e61992f244
Octavian Regatun <octavian.regatun@outlook.com>
2023-03-07 00:24:48 +0200

    feat: fix navbar titles

--- src/utils/navbar.ts
+++ src/utils/navbar.ts
@@ -1,17 +1,9 @@
 export function getNavbarTitle(route: string) {
-  switch (route) {
-    case "/dashboard":
-      return "Dashboard"
-    case "/social":
-      return "Social"
-    case "/profile":
-      return "Profile"
-    case "/groups":
-      return "Groups"
-    case "/groups/create":
-      return "Create Group"
-
-    default:
-      return "Sample Title"
-  }
+  if (route.startsWith("/dashboard")) return "Dashboard"
+  if (route.startsWith("/social")) return "Social"
+  if (route.startsWith("/profile")) return "Profile"
+  if (route.startsWith("/groups/create")) return "Create Group"
+  if (route.startsWith("/groups")) return "Groups"
+  if (route.startsWith("/events/create")) return "Create Event"
+  if (route.startsWith("/events")) return "Events"
 }
