6f729f1c422bef83b4cc473c1d71313222937849
Octavian Regatun <octavian.regatun@outlook.com>
2023-05-03 03:25:45 +0300

    feat: add "add friend" button on user profiles

--- src/pages/users/[username].tsx
+++ src/pages/users/[username].tsx
@@ -14,9 +14,20 @@ const UserPage: React.FC = () => {
     }
   )
 
+  const isFriendQuery = api.friendships.isFriend.useQuery(
+    { userId: getUserQuery.data?.id as string },
+    {
+      enabled: !!getUserQuery.data?.id,
+    }
+  )
+
+  const addFriendMutation = api.friendships.addFriend.useMutation()
+
+  console.log(router.query.username)
+
   return (
     <RequireAuth>
-      <Layout>
+      <Layout className="p-0">
         {getUserQuery.data && (
           <>
             <Image
@@ -27,16 +38,28 @@ const UserPage: React.FC = () => {
             />
             <div className="relative -top-8 flex h-full flex-col items-center rounded-t-3xl bg-white">
               <Image
-                className="absolute -top-16 rounded-full transition-all hover:brightness-75"
-                width={128}
-                height={128}
+                className="absolute -top-24 rounded-full transition-all hover:brightness-75"
+                width={196}
+                height={196}
                 src={getUserQuery.data.image}
                 alt="profile"
               />
-              <p className="mt-20 text-center text-2xl">
+              <p className="mt-32 text-center text-2xl">
                 {getUserQuery.data.firstName} {getUserQuery.data.lastName}
               </p>
               <p>{getUserQuery.data.username}</p>
+              {isFriendQuery.data === false && (
+                <button
+                  className="rounded-lg border border-teal-600 p-2"
+                  onClick={() =>
+                    addFriendMutation.mutate({
+                      recipientId: getUserQuery.data?.id as string,
+                    })
+                  }
+                >
+                  Add Friend
+                </button>
+              )}
             </div>
           </>
         )}
