50f83009ad617c14156049efc8c377db10dbea9e
Octavian Regatun <octavian.regatun@outlook.com>
2023-09-27 22:01:50 +0300

    feat: add ability to leave group

---
+++ src/components/ErrorCard.tsx
@@ -0,0 +1,16 @@
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import { Alert, AlertDescription, AlertTitle } from "./shadcn/Alert";
+
+interface Props {
+  message: string;
+}
+
+export function ErrorCard({ message }: Props) {
+  return (
+    <Alert variant="destructive" className="bg-red-100">
+      <ExclamationTriangleIcon className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>{message}</AlertDescription>
+    </Alert>
+  );
+}
--- src/services/group-members.ts
+++ src/services/group-members.ts
@@ -1,8 +1,17 @@
 import { backendAxios } from "@/utilities/axios";
-import { Group } from "./groups";
+import { Group, GroupMember } from "./groups";
 
 export const groupMembersService = {
   async delete(id: number) {
     return await backendAxios.delete(`group-members/${id}`);
+  },
+  async findMeByGroupId(groupId: number) {
+    return await backendAxios.get(`group-members/groups/${groupId}/me`);
+  },
+  async accept(groupId: number) {
+    return await backendAxios.patch<GroupMember>(`group-members/${groupId}`);
+  },
+  getMemberFromGroupByUserId(group: Group, userId: number) {
+    return group.groupMembers.find(member => member.userId === userId);
   },
 };
