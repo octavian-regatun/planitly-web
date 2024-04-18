dd1e4a426eaca6c9b042664813975ea57d4391fb
Octavian Regatun <octavian.regatun@outlook.com>
2024-04-18 21:37:22 +0300

    feat: update shadcn components

--- src/components/shadcn/use-toast.ts
+++ src/components/shadcn/use-toast.ts
@@ -1,3 +1,6 @@
+"use client"
+
+// Inspired by react-hot-toast library
 import * as React from "react"
 
 import type {
@@ -25,7 +28,7 @@ const actionTypes = {
 let count = 0
 
 function genId() {
-  count = (count + 1) % Number.MAX_VALUE
+  count = (count + 1) % Number.MAX_SAFE_INTEGER
   return count.toString()
 }
 
